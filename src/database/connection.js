9884f1aecf968b6a30a434b803a0653684f34b31
evans656-04 <evanssalgadovilchez@gmail.com>
2025-04-01 11:18:27 -0600

    comentarios en connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,13 @@
 import sql from 'mssql';
 
 const dbSettings = {
-    user: 'evans',
-    password: '6565',
-    server: 'EVANSPC\MSSQLSERVER01',  //pongan el nombre de su servidor
-    database: 'Zuleyka',
-    port: 1434, //probar si funciona este puerto, sino ponga el puerto de su sql server
+    user: 'evans2',
+    password: '1234',
+    server: '127.0.0.1',  //pongan el nombre de su servidor o la dirección IP si no funciona el nombre
+    database: 'Zuleyka',  //usar EXEC xp_readerrorlog 0, 1, N'Server is listening on'; en MSSQL para ver el ip y el puerto que usa
+    port: 57794, //en mi caso el puerto es 57794, pero puede variar en cada caso
+                // configurar el puerto en el firewall de windows para que no lo bloquee
+                //configurar en sql server para que acepte conexiones TCP/IP y poner el puerto que ocupa
     options: {
         encrypt: false,
         trustServerCertificate: true
